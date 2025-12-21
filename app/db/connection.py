8c7effdba8427c72e17a74e242067837fb8005b3
DingHY <3023265978@qq.com>
2025-12-21 16:30:18 +0800

    4

--- app/db/connection.py
+++ app/db/connection.py
@@ -5,16 +5,31 @@ from mysql.connector import Error
 logger = logging.getLogger()
 
 def get_connection():
-    """获取数据库连接"""
+
+    # host="223.26.59.193"
+    # user="admin"
+    # password="Dhy20040501-"  # 密码
+    # database="signinsystem"   # 数据库名
+    # host="localhost"
+    # user="root"
+    # password="Dhy20040501-"  # 密码
+    # database="signinsystem"   # 数据库名
+    host="host.docker.internal"
+    user="signin"
+    password="Dhy20040501-"  # 密码
+    database="signin"   # 数据库名
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='860066',  # 密码
-            database='signinsystem'   # 数据库名
+            host=host,
+            user=user,
+            password=password,
+            database=database
         )
         logger.info("数据库连接成功")
         return connection
     except Error as e:
-        print(f"数据库连接失败: {e}")
+        logger.info(f"数据库连接信息: {host, user, password, database}")
+        logger.error(f"数据库连接失败: {e}")
         return None
+    
+
